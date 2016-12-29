77799454248d883bfb6cbcb4c5adc7cfa634f630
acemile <adrianous@hotmail.com>
2016-12-29 02:06:46 -0500

    File Creation
    Created the cv file

---
+++ cv.php
@@ -0,0 +1,286 @@
+<DOCTYPE! html>
+<html>
+    <head>
+        <title>Ismail Qaznili CV</title>
+        <meta charset="utf-8">
+        <meta name="description" content="IQ CV is a resume for Ismail Qaznili.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- Custome CSS -->
+        <link rel="stylesheet" href="cv.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body class="container">
+        <div id="header" class="row">
+            <div id="logo" class="col-sm-4 text-center">
+                <h1><strong>Ismail Qaznili</strong></h1>
+                <p><em>Network Security Architect</em></p>
+            </div>
+            <div id="blurp" class="col-sm-8 text-left">
+                <p><em>"Without the proper protection, our networks are vulnerable to black-hat hackers, identity thieves and spammers. There are few people who have the skills and education needed to ward off the enemies lurking behind computer screens, ready to thwart firewalls, infiltrate networks and corrupt servers [...] Quite simply, there are not enough people qualified with this high level skill set [...] in Cisco, Microsoft, Redhat, VMware and many other vendors and specialized knowledge in everything from firewalls to server infrastructure."</em></p>
+            </div>
+        </div>
+        <div id="banner" class="row bg-primary">
+            <div class="col-sm-4"><span class="glyphicon glyphicon-phone"></span> <a href="tel: +1 (226) 700-0854">+1 (226) 700-0854</a></div>
+            <div class="col-sm-4"><span class="glyphicon glyphicon-envelope"></span> <a href="mailto: adrianous@hotmail.com">adrianous@hotmail.com</a></div>
+            <div class="col-sm-4"><span class="glyphicon glyphicon-globe"></span> <a href="http://iqdesigns.info" target="_blank">iqdesigns.info</a></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div id="skills" class="row">
+                    <h4 class="col-xs-12"><strong>SKILLS</strong></h4>
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                    </div>
+                    <div class="col-xs-7">Office&trade;</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">Photoshop&trade;</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">HTML, CSS, JScript</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">PHP, SQL</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">Virtualization</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">VPN</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">SketchUp&trade;</div>
+                    
+                </div>
+                <div id="languages" class="row">
+                    <h4 class="col-xs-12"><strong>LANGUAGES</strong></h4>
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">Arabic</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">English</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">Spanish</div>
+                    
+                    <div class="col-xs-5">
+                        <span class="glyphicon glyphicon-star"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                        <span class="glyphicon glyphicon-star-empty"></span>
+                    </div>
+                    <div class="col-xs-7">Japanese</div>
+                    
+                </div>
+                
+                <div id="education" class="row">
+                    <div class="col-xs-12">
+                        <h4><strong>EDUCATION</strong></h4>
+                        <p>
+                        <strong>Network and Security Architecture - 2016</strong>
+                        <br>
+                        <em>Fanshawe College, London ON, Canada</em>
+                        <br>
+                        Post-graduate degree in the art of designing enterprise networks to server business needs such as virtualization, VPN, wireless networks and secure webdesign.
+                        </p>
+                        <p>
+                        <strong>Master of Architecture in Urban Design - 2009</strong>
+                        <br>
+                        <em>The University of Nottingham, Nottingham, England</em>
+                        <br>
+                        Master of Architecture in Urban Design focusing on green and sustainable design principles in designing urban areas for high standards of living. 
+                        </p>
+                        <p>
+                        <strong>Bachelor of Urban and Regional Planning - 2006</strong>
+                        <br>
+                        <em>King Abdul-Aziz University, Jeddah, Saudi Arabia</em>
+                        </p>
+                    </div>
+                    
+                </div>
+                
+                <div id="certs" class="row">
+                    <div class="col-xs-12">
+                        <h4><strong>CERTIFICATES</strong></h4>
+                        <p>
+                        <strong>ACE TESOL Certificate - 2011</strong>
+                        <br>
+                        <em>London Language Institute, London ON, Canada</em>
+                        </p>
+                    </div>
+                </div>
+                
+                <div id="likes" class="row">
+                    <h4 class="col-xs-12"><strong>LIKES</strong></h4>
+                    
+                    <div class="col-xs-3">
+                        <div><span class="glyphicon glyphicon-blackboard"></span></div>
+                        <p>Online<br>Learning</p>
+                    </div>
+                    <div class="col-xs-3">
+                        <div><span class="glyphicon glyphicon-knight"></span></div>
+                        <p>Board<br>Games</p>
+                    </div>
+                    <div class="col-xs-3">
+                        <div><span class="glyphicon glyphicon-cutlery"></span></div>
+                        <p>Dining<br>Out</p>
+                    </div>
+                    <div class="col-xs-3">
+                        <div><span class="glyphicon glyphicon-book"></span></div>
+                        <p>Free<br>Reading</p>
+                    </div>
+                    <div class="col-xs-3">
+                        <div><span class="glyphicon glyphicon-music"></span></div>
+                        <p>Latin<br>Dance</p>
+                    </div>
+                    <div class="col-xs-3">
+                        <div><span class="glyphicon glyphicon-scale"></span></div>
+                        <p>Fitness<br>Workout</p>
+                    </div>
+                    <div class="col-xs-3">
+                        <div><span class="glyphicon glyphicon-bullhorn"></span></div>
+                        <p>Singing<br>Karaoke</p>
+                    </div>
+                    <div class="col-xs-3">
+                        <div><span class="glyphicon glyphicon-facetime-video"></span></div>
+                        <p>YouTube&trade;<br>Videos</p>
+                    </div>
+                    
+                </div>
+            </div>
+            
+            <div id="exp" class="col-sm-6 bg-success">
+                
+                <h4><strong>EXPERINENCE</strong></h4>
+                <p>
+                    <strong>Student Ambassador – International Office</strong> <span>[May 2016 - Jan 2017]</span>
+                    <br>
+                    <em>Fanshawe College, London ON, Canada</em>
+                    <br>
+                    <span>Providing customers services to international students in English, Arabic and limited Spanish. Handling walk-in visits and appointments through out the day using Outlook and data collection google forms. Provide general guidance to students and assistance with all online Fanshawe college platforms. </span>
+                </p>
+                <p>
+                    <strong>IT Manager / MCAT & TOEFL iBT Test Centre Administrator</strong> <span>[January 2011 - January 2016]</span>
+                    <br>
+                    <em>London Language Institute, London ON, Canada</em>
+                    <br>
+                    <span>Design and operating all web application and website for local costumers and international partners via different CMSs and hard code design solutions. Maintaining all computers and network connectivity for classes needs and for all major online testing such as TOEFL and MCAT.</span>
+                </p>
+                <p>
+                    <strong>Urban Planner – City Planning Department</strong>
+                    <br>
+                    <em>Al-Madinah Regional Municipality, Saudi Arabia</em>
+                    <br>
+                    <span>January 2007 - January 2008</span>
+                </p>
+                
+            </div>
+            
+            <div id="volunteer" class="col-sm-6 bg-success">
+                
+                <h4><strong>VOLUNTEERING</strong></h4>
+                <p>
+                    <strong>Fanshawe College Latin Arts of Dance Club</strong>
+                    <br>
+                    <em>Fanshawe College, London ON, Canada</em>
+                    <br>
+                    <span>[September 2016 - November 2016]</span>
+                    <br>
+                    Club president managing registration and class planning and dance instructor covering foundations of Salsa and Bachata dances.
+                </p>
+                <p>
+                    <strong>Peer 2 Peer – International Office</strong>
+                    <br>
+                    <em>Fanshawe College, London ON, Canada</em>
+                    <br>
+                    <span>[May 2016 - August 2016]</span>
+                    <br>
+                    IETLS Test Preparation covering Reading, Writing, Speaking and Listening skills training and practice test procotring.
+                </p>
+                <p>
+                    <strong>The 3rd International Exhibition & Conference on Higher Education</strong>
+                    <br>
+                    <em>Riyadh, Saudi Arabia</em>
+                    <br>
+                    <span>[April 2012]</span>
+                    <br>
+                    Represented London Language Institute for the duration for the exhibition helping with promoting the institute to the visitors using both Arabic and English, and working alongside many representatives from other Canadian ESL schools
+                </p>
+                
+            </div>
+        </div>
+
+        <div id="quote" class="row text-center">
+            <p><em>"There is no knowledge that is not power"</em></p>
+            <p> Ralph Waldo Emerson </p>
+        </div>
+
+        <br>
+    </body>
+</html>
